1b5a82999757e47707cfd749af08380936d62ada
Mauricio <mauricioluan2023@gmail.com>
2025-11-01 14:46:46 -0300

    feat: implementa login

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,7 +1,27 @@
-<template lang="">
-  <div>Login</div>
+<template>
+  <form @submit.prevent="login">
+    <label for="username">username: </label>
+    <input id="username" type="text" v-model="userName" />
+    <label for="password">password: </label>
+    <input id="password" type="password" v-model="password" />
+    <button>Login</button>
+  </form>
 </template>
+
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      userName: '',
+      password: '',
+    }
+  },
+
+  methods: {
+    login() {
+      this.$store.dispatch('auth/login')
+      this.$router.push({ name: 'dashboard' })
+    },
+  },
+}
 </script>
-<style lang=""></style>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,7 +2,7 @@ export default {
   namespaced: true,
 
   state: {
-    isLoggedIn: true,
+    isLoggedIn: false,
     currentUser: '',
   },
 
